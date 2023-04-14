d0164ac24cff7145b651040ba90a75d7e68f8cb2
Gabriela Florêncio <perflogabi@gmail.com>
2023-04-13 23:02:37 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import { Dashboard } from './pages/Dashboard/Dashboard';
-import { Layout } from './components/Layout/Layout'
+import { Layout } from './components/Layout/Layout';
+import BoardPage from './pages/Board/Board';
+import Calendar from './pages/Calendar/Calendar';
+import DataGrid from './pages/DataGrid/DataGrid';
 
 function App() {
   return (
-    <div className="dashboard">
+    <div id="dashboard">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Layout/>}/>
-            <Route path='dashboard' eleemnt={<Dashboard/>}/>
+          <Route path='/' element={<Layout/>}>
+            <Route path='dashboard' element={<Dashboard/>}/>
+            <Route path="calendar" element={<Calendar/>}/>
+            <Route path="board" element={<BoardPage/>}/>
+            <Route path="users" element={<DataGrid/>}/>
+          </Route>
+
         </Routes>
       </BrowserRouter>
     </div>
